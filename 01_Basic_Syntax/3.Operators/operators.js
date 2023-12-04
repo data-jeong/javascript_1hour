7c9e2727d4e0b4f3d10ee54fca5b04c65d7ddac5
data-jeong <lightyear94122@gmail.com>
2023-12-04 17:40:38 +0900

    operators

---
+++ 01_Basic_Syntax/3.Operators/operators.js
@@ -0,0 +1,22 @@
+// 산술 연산자
+let sum = 10 + 5; // 덧셈
+let difference = 10 - 5; // 뺄셈
+let product = 10 * 5; // 곱셈
+let quotient = 10 / 5; // 나눗셈
+
+// 할당 연산자
+let number = 10;
+number += 5; // number = number + 5 와 동일
+
+// 비교 연산자
+let isEqual = 10 == '10'; // 값만 비교 (true)
+let isIdentical = 10 === '10'; // 값과 타입 모두 비교 (false)
+
+// 논리 연산자
+let isAdult = age > 18 && age < 65; // AND 연산자: 두 조건이 모두 참일 때 true
+let isTeenager = age > 13 || age < 19; // OR 연산자: 두 조건 중 하나라도 참일 때 true
+
+// 조건 (삼항) 연산자
+let age = 20;
+let ageGroup = (age >= 18) ? "Adult" : "Minor"; // 조건이 참이면 "Adult", 거짓이면 "Minor"
+
